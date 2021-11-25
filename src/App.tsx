a65d2398c88ee88d1649b237c4e93816f5569b89
Jingu Kim <wlsrn3684@naver.com>
2021-11-25 20:37:02 +0900

    Add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
-import Home from './component/home'
+import Codi from './component/codi'
+import Footer from './component/layout/footer'
+import Header from './component/layout/header'
+import Login from './component/login'
 import Mypage from './component/mypage'
 import Product from './component/product'
 import SelfDiagnosis from './component/selfDiagnosis'
+import Signup from './component/signup'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
-        <Route path="/service" component={Home} />
-        <Route path="/mypage" component={Mypage} />
-        <Route path="/product" component={Product} />
-        <Route path="/self" component={SelfDiagnosis} />
+        <Route exact path="/" component={Product} />
+        <Route exact path="/product" component={Product} />
+        <Route exact path="/mypage" component={Mypage} />
+        <Route exact path="/self" component={SelfDiagnosis} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/codi" component={Codi} />
       </Router>
+      <Footer />
     </div>
   )
 }
